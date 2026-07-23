0beabc8c13aa1bc6b5748f0ae11f0f6672efcc63
Kyle Bergman <60984164+bergmankyle99@users.noreply.github.com>
2026-07-23 15:02:36 -0700

    more comments

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 from dotenv import load_dotenv
 load_dotenv()
+#get database url from .env
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+#create postgresql engine using the database URL
 engine = create_engine(DATABASE_URL)
+#create local session via session maker, no auto commit, no autoflush, and bind the session to the engine
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
+#create Base model
 Base = declarative_base()
